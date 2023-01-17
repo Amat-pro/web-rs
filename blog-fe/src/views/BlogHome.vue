611ac13d0004792e57c4a8157395712f279d0736
maomao <2892798998@qq.com>
2023-01-17 16:13:23 +0800

    perf(fe): perf code

--- blog-fe/src/views/BlogHome.vue
+++ blog-fe/src/views/BlogHome.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="common-layout">
+  <div class="blog-home">
     <el-container>
       <el-header></el-header>
       <el-main>
-        <el-row class="mb-4">
-          <el-button type="success" round>登录</el-button>
-          <el-button type="success" round>注册</el-button>
+        <el-row class="login-register">
+          <el-button type="success" round @click="login">登录</el-button>
+          <el-button type="success" round @click="register">注册</el-button>
+          <el-button type="success" round @click="viewArticle">游客</el-button>
         </el-row>
       </el-main>
       <el-footer></el-footer>
@@ -17,6 +18,17 @@
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "BlogHome",
+  methods: {
+    login() {
+      this.$router.push("/login");
+    },
+    register() {
+      this.$router.push("/register");
+    },
+    viewArticle() {
+      this.$router.push("/article");
+    },
+  },
   mounted() {
     console.log("todo check token");
     let token_valid = true;
